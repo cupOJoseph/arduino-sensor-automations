528341fb10e397f6500080d9c56d3dab666034a5
jschiarizzi <jschiarizzi@gwu.edu>
2018-07-21 17:26:56 -0400

    add oxygen sensor

--- sensoroutline-Joseph/combined-sensor/combined-sensor.ino
+++ sensoroutline-Joseph/combined-sensor/combined-sensor.ino
@@ -19,14 +19,31 @@ void setup(){
 
 void loop() {
   // put your main code here, to run repeatedly:
+  String status_string = ""; //a string to build a status with and print out at the end.
 
+  //example using get temperature function
   double temp = getTemperature();
+  status_string += "temp = ";
+  status_string += temp;
+
+  //example using get humidity function
   double humi = getHumidity();
-  Serial.println("temp = %d", temp);
-  Serial.println("humidity = %d", humi);
+  status_string += "; humidity = ";
+  status_string += humi;
+
+  //example using get oxygen
+  
+  
+  Serial.println(status_string);
   
   delay(1000);
 }
+
+// ============================================================= 
+// |                      HELPER FUNCTIONS                     |
+// |                                                           |
+// =============================================================
+//This area below for Joseph's custom functions.
 
 /* This function will record the temperature using the
  *  sensor assigned to pin TemperatureAndHumidity_pin.
@@ -38,8 +55,54 @@ double getTemperature(){
   return DHT.temperature;
 }
 
+/* This function will record the Humidity using the
+ * sensor assigned to pin TemperatureAndHumidity_pin.
+ * Then it will return the current humidity as a double in celcius.
+ * Sometimes this will return -999. Please check this.
+ */
 double getHumidity(){
    int check = DHT.read11(TemperatureAndHumidity_pin);
   return DHT.humidity;
 }
+
+
+/**get the oxygen level of the air.
+ * I think this sensor is just for air.
+ * 
+ */
+ float getOxygenInAir(){
+    float Vout =0;
+    Vout = readO2VoutAir();
+    return readConcentrationAir();
+
+ }
+
+
+//================ O2 sensor helper functions ===================/
+// THIS SECTION SECTION UN TESTED!
+//
+float readO2VoutAir()
+{
+    long sum = 0;
+    for(int i=0; i<32; i++)
+    {
+        sum += analogRead(OxygenSensorPin);
+    }
+    sum >>= 5;
+    float MeasuredVout = sum * (VRefer / 1023.0);
+    return MeasuredVout;
+}
+
+float readConcentrationAir()
+{
+    // Vout samples are with reference to 3.3V
+    float MeasuredVout = readO2VoutAir();
+
+    //float Concentration = FmultiMap(MeasuredVout, VoutArray,O2ConArray, 6);
+    //when its output voltage is 2.0V,
+    float Concentration = MeasuredVout * 0.21 / 2.0;
+    float Concentration_Percentage=Concentration*100;
+    return Concentration_Percentage;
+}
+// ================================================== ^End O2 helpers =================================== //
 
---
+++ sensoroutline-Joseph/gasOxygen-example/gasOxygen-example.ino
@@ -0,0 +1,55 @@
+//get gas O2 from sensor
+// http://wiki.seeedstudio.com/Grove-Gas_Sensor-O2/
+
+const float VRefer = 3.3;// voltage of adc reference
+
+const int OxygenSensorPin = A5;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+//The O2 sensor writes votage directly, must be converted to a useful number
+void loop() {
+  float x = getOxygenInAir();
+  delay(500);
+}
+
+/**get the oxygen level of the air.
+ * I think this sensor is just for air.
+ * 
+ */
+ float getOxygenInAir(){
+    float Vout =0;
+    Vout = readO2VoutAir();
+    return readConcentrationAir();
+
+ }
+
+
+//================ O2 sensor helper functions ===================/
+// THIS SECTION SECTION UN TESTED!
+//
+float readO2VoutAir()
+{
+    long sum = 0;
+    for(int i=0; i<32; i++)
+    {
+        sum += analogRead(OxygenSensorPin);
+    }
+    sum >>= 5;
+    float MeasuredVout = sum * (VRefer / 1023.0);
+    return MeasuredVout;
+}
+
+float readConcentrationAir()
+{
+    // Vout samples are with reference to 3.3V
+    float MeasuredVout = readO2VoutAir();
+
+    //float Concentration = FmultiMap(MeasuredVout, VoutArray,O2ConArray, 6);
+    //when its output voltage is 2.0V,
+    float Concentration = MeasuredVout * 0.21 / 2.0;
+    float Concentration_Percentage=Concentration*100;
+    return Concentration_Percentage;
+}
