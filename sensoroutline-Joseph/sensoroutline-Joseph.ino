579269ceec3c9e514c626198e36a0e2f3fae968f
jschiarizzi <jschiarizzi@gwu.edu>
2018-07-21 13:43:42 -0400

    add temp read and compile library to read

--- sensoroutline-Joseph/sensoroutline-Joseph.ino
+++ sensoroutline-Joseph/sensoroutline-Joseph.ino
@@ -1,11 +1,18 @@
-//define global variables.
+//define other files to load.
+//depending on file location you may need to use "<dht.h>" instead of ""dht.h""
+#include "dht.h"
 
-void setup() {
-  // put your setup code here, to run once:
+//define global scope variables here
+dht DHT;
+
+#define DHT11_PIN 7
+
+void setup(){
+  Serial.begin(9600);
+  // put your setup code here, to run once when the program is sent:
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
